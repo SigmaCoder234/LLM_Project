c941e259523980b9fbb8fd41e6e30ca8fab600fa
Илья <144475505+SigmaCoder234@users.noreply.github.com>
2025-10-29 17:04:29 +0300

    Delete token.py

--- token.py
+++
@@ -1,3 +0,0 @@
-# GigaChat Access Token
-# Замените на реальный токен если нужен
-TOKEN = "eyJjdHkiOiJqd3QiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwiYWxnIjoiUlNBLU9BRVAtMjU2In0.tvaS8A1YHU924PE0rtX45KYeI8rqSmP3yP..."
